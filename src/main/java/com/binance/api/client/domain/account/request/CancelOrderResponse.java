3394bc14b183da7bfc69a89da3e04f01ccbbd2f8
Alfredo Ferreira <15140256+aaoferreira@users.noreply.github.com>
2020-08-07 14:52:37 +0200

    Add cancel order reponse fields

--- src/main/java/com/binance/api/client/domain/account/request/CancelOrderResponse.java
+++ src/main/java/com/binance/api/client/domain/account/request/CancelOrderResponse.java
@@ -1,6 +1,9 @@
 package com.binance.api.client.domain.account.request;
 
 import com.binance.api.client.constant.BinanceApiConstants;
+import com.binance.api.client.domain.OrderSide;
+import com.binance.api.client.domain.OrderStatus;
+import com.binance.api.client.domain.OrderType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
@@ -16,12 +19,23 @@ public class CancelOrderResponse {
 
   private String origClientOrderId;
 
-  private String orderId;
+  private Long orderId;
 
   private String clientOrderId;
 
-  private String status;
+  private String price;
+
+  private String stopPrice;
+
+  private String origQty;
+
   private String executedQty;
+
+  private OrderStatus status;
+
+  private OrderType type;
+
+  private OrderSide side;
 
   public String getSymbol() {
     return symbol;
@@ -41,17 +55,11 @@ public class CancelOrderResponse {
     return this;
   }
 
-  public String getStatus() {
-    return status;
-  }
-  public String getExecutedQty() {
-    return executedQty;
-  }
-
-  public String getOrderId() {
+  public Long getOrderId() {
     return orderId;
   }
-  public CancelOrderResponse setOrderId(String orderId) {
+
+  public CancelOrderResponse setOrderId(Long orderId) {
     this.orderId = orderId;
     return this;
   }
@@ -65,6 +73,69 @@ public class CancelOrderResponse {
     return this;
   }
 
+  public String getPrice() {
+    return price;
+  }
+
+  public CancelOrderResponse setPrice(String price) {
+    this.price = price;
+    return this;
+  }
+
+  public String getStopPrice() {
+    return stopPrice;
+  }
+
+  public CancelOrderResponse setStopPrice(String stopPrice) {
+    this.stopPrice = stopPrice;
+    return this;
+  }
+
+  public String getOrigQty() {
+    return origQty;
+  }
+
+  public CancelOrderResponse setOrigQty(String origQty) {
+    this.origQty = origQty;
+    return this;
+  }
+
+  public String getExecutedQty() {
+    return executedQty;
+  }
+
+  public CancelOrderResponse setExecutedQty(String executedQty) {
+    this.executedQty = executedQty;
+    return this;
+  }
+
+  public OrderStatus getStatus() {
+    return status;
+  }
+
+  public CancelOrderResponse setStatus(OrderStatus status) {
+    this.status = status;
+    return this;
+  }
+
+  public OrderType getType() {
+    return type;
+  }
+
+  public CancelOrderResponse setType(OrderType type) {
+    this.type = type;
+    return this;
+  }
+
+  public OrderSide getSide() {
+    return side;
+  }
+
+  public CancelOrderResponse setSide(OrderSide side) {
+    this.side = side;
+    return this;
+  }
+
   @Override
   public String toString() {
     return new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
@@ -72,6 +143,13 @@ public class CancelOrderResponse {
         .append("origClientOrderId", origClientOrderId)
         .append("orderId", orderId)
         .append("clientOrderId", clientOrderId)
+        .append("price", price)
+        .append("stopPrice", stopPrice)
+        .append("origQty", origQty)
+        .append("executedQty", executedQty)
+        .append("status", status)
+        .append("type", type)
+        .append("side", side)
         .toString();
   }
 }
